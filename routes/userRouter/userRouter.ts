3aa1c7140c30479cc20599a2389d6271684e03fb
Fedor <mfs9408@yandex.ru>
2022-01-09 13:03:39 -0500

    Removes validation.

--- routes/userRouter/userRouter.ts
+++ routes/userRouter/userRouter.ts
@@ -1,14 +1,8 @@
 import { Router } from "express";
-import { body } from "express-validator";
 import userController from "../../controllers/userController";
 
 const router = Router();
-router.post(
-  "/registration",
-  body("email").isEmail(),
-  body("password").isLength({ min: 3, max: 32 }),
-  userController.userRegistration
-);
+router.post("/registration", userController.userRegistration);
 router.post("/login", userController.userAuthorization);
 router.get("/logout", userController.userLogout);
 router.get("/refresh", userController.refreshToken);
